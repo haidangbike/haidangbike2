2ab165e5eb55e060805ac436ed928c7de2ef99bb
HaiDangBike <haidangbike@gmail.com>
2015-02-05 11:34:36 +0700

    Create searchform.php

---
+++ searchform.php
@@ -0,0 +1,12 @@
+<div class="col-md-6">
+	<div id="search">
+		<div class="input-group">
+		  <input type="text" class="form-control input-lg" placeholder="Tìm kiếm">
+		  <span class="input-group-btn">
+			<button class="btn btn-lg" type="button">
+				<i class="fa fa-search"></i>
+			</button>
+		  </span>
+		</div>
+	</div>	
+</div>
